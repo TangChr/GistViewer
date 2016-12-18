d41999f50c992c1f028df544524f275b8acdc8bf
Christian Tang <git@tcdev.dk>
2016-12-18 03:12:49 +0100

    Fixed indentation

--- app/js/index.js
+++ app/js/index.js
@@ -1,47 +1,47 @@
-var electron = require('electron');
+var electron    = require('electron');
 var ipcRenderer = electron.ipcRenderer;
-var jhub = require('jhub');
+var jhub        = require('jhub');
 
 var load 	 = document.querySelector('#load');
 var close 	 = document.querySelector('#close');
 var gistList = document.querySelector('#gist-list');
 
 load.addEventListener('click', function () {
-		jhub.init('TangChr');
+    jhub.init('TangChr');
     jhub.gists(function(gists) {
-			var gistItems = '';
-			for (var g in gists) {
-				gistItems += '<li><a class="gist-link" id="'+gists[g].id+'">'+gists[g].description+'</a></li>';
-			}
-			gistList.innerHTML = gistItems;
-			var gistLinks = document.querySelectorAll('.gist-link');
-			for(var l = 0; l < gistLinks.length; l++) {
-				var link = gistLinks[l];
-				var id = link.attributes['id'].value;
-				prepareGistLink(link, id);
-			}
-		});
+        var gistItems = '';
+        for (var g in gists) {
+            gistItems += '<li><a class="gist-link" id="'+gists[g].id+'">'+gists[g].description+'</a></li>';
+        }
+        gistList.innerHTML = gistItems;
+        var gistLinks = document.querySelectorAll('.gist-link');
+        for(var l = 0; l < gistLinks.length; l++) {
+            var link = gistLinks[l];
+            var id = link.attributes['id'].value;
+            prepareGistLink(link, id);
+        }
+    });
 });
 
 function prepareGistLink(gistLink, gistId) {
-	gistLink.addEventListener('click', function() {
-			openGist(gistId);
-	});
+    gistLink.addEventListener('click', function() {
+openGist(gistId);
+});
 }
 
 function openGist(gistId) {
-	var gist = jhub.gist(gistId);
-	gist.get(function(info) {
-		var files = 'Files: ';
-		var langs = 'Languages: ';
-		for(var f = 0; f < info.files.length; f++) {
-			files += info.files[f].name+', ';
-			langs += info.files[f].language+', ';
-		}
-		alert(info.description + '\n' + files.substr(0, files.length-2) + '\n' + langs.substr(0, langs.length-2));
-	});
+    var gist = jhub.gist(gistId);
+    gist.get(function(info) {
+        var files = 'Files: ';
+        var langs = 'Languages: ';
+        for(var f = 0; f < info.files.length; f++) {
+            files += info.files[f].name+', ';
+            langs += info.files[f].language+', ';
+        }
+        alert(info.description + '\n' + files.substr(0, files.length-2) + '\n' + langs.substr(0, langs.length-2));
+    });
 }
 
 close.addEventListener('click', function () {
-	ipcRenderer.sendSync('close-main-window');
+    ipcRenderer.sendSync('close-main-window');
 });
--- main.js
+++ main.js
@@ -6,16 +6,15 @@ var mainWindow;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-                             width: 640,
-                             height: 490,
-                             frame: false,
-                             resizable: true
-                            });
-
-  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+        width: 640,
+        height: 490,
+        frame: false,
+        resizable: true
+    });
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 });
 
 ipcMain.on('close-main-window', function() {
