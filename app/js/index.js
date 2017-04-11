41b191b9d5ae0fad76a7014f0333a2cbb409f823
Christian Tang <git@tcdev.dk>
2017-04-11 12:06:56 +0200

    Relocated some functions to "satisfy" jshint

--- app/js/index.js
+++ app/js/index.js
@@ -6,29 +6,6 @@ var load 	 = document.querySelector('#load');
 var close 	 = document.querySelector('#close');
 var gistList = document.querySelector('#gist-list');
 
-load.addEventListener('click', function () {
-    jhub.init('TangChr');
-    jhub.gists(function(gists) {
-        var gistItems = '';
-        for (var g in gists) {
-            gistItems += '<li><a class="gist-link" id="'+gists[g].id+'">'+gists[g].description+'</a></li>';
-        }
-        gistList.innerHTML = gistItems;
-        var gistLinks = document.querySelectorAll('.gist-link');
-        for(var l = 0; l < gistLinks.length; l++) {
-            var link = gistLinks[l];
-            var id = link.attributes['id'].value;
-            prepareGistLink(link, id);
-        }
-    });
-});
-
-function prepareGistLink(gistLink, gistId) {
-    gistLink.addEventListener('click', function() {
-openGist(gistId);
-});
-}
-
 function openGist(gistId) {
     var gist = jhub.gist(gistId);
     gist.get(function(info) {
@@ -41,6 +18,29 @@ function openGist(gistId) {
         alert(info.description + '\n' + files.substr(0, files.length-2) + '\n' + langs.substr(0, langs.length-2));
     });
 }
+
+function prepareGistLink(gistLink, gistId) {
+    gistLink.addEventListener('click', function() {
+openGist(gistId);
+});
+}
+
+load.addEventListener('click', function () {
+    jhub.init('TangChr');
+    jhub.gists(function(gists) {
+        var gistItems = '';
+        for (var g in gists) {
+            gistItems += '<li><a class="gist-link" id="'+gists[g].id+'">'+gists[g].description+'</a></li>';
+        }
+        gistList.innerHTML = gistItems;
+        var gistLinks = document.querySelectorAll('.gist-link');
+        for(var l = 0; l < gistLinks.length; l++) {
+            var link = gistLinks[l];
+            var id = link.attributes.id;
+            prepareGistLink(link, id);
+        }
+    });
+});
 
 close.addEventListener('click', function () {
     ipcRenderer.sendSync('close-main-window');
